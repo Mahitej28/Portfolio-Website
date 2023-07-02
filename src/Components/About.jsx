ca89cfdc5245a9851f9c84d5a61ab437fa3726cc
Mahima Churi <mahimachuri.28@gmail.com>
2023-07-02 21:54:08 +0530

    About Section Updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-gradient-to-b from-cyan-900 to-black text-white pb-5'>
-       <div className=' max-w-screen-lg flex flex-col justify-center mx-auto p-4 w-full h-full'>
+       <div className=' max-w-screen-lg flex flex-col justify-center mx-auto pt-10 px-4 md:p-4 w-full h-full'>
         <div className='pb-8'>
             <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>About Me</p>
         </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,8 +4,8 @@ import {MdKeyboardDoubleArrowRight} from 'react-icons/md';
 
 const Home = () => {
   return (
-    <div name ='home' className=' h-screen w-full bg-gradient-to-b from-black to-cyan-900'>
-        <div className=' max-w-screen-lg flex flex-col items-center justify-center px-4 h-full mx-auto md:flex-row'>
+    <div name ='home' className='h-screen w-full bg-gradient-to-b from-black to-cyan-900 overflow-hidden'>
+        <div className='max-w-screen-lg flex flex-col items-center justify-center px-4 h-full mx-auto md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className=" text-4xl sm:text-7xl font-bold text-white">Hey! I'm Mahima Churi</h2>
                 <p className=' text-gray-400 py-4 max-w-md'>Mumbai based Full Stack Web Developer. With every line of code, I endeavor to create efficient web applications.</p>
@@ -19,7 +19,7 @@ const Home = () => {
                 </div>
             </div>
             <div>
-                <img src={heroImage} alt='My profile' className=' rounded-2xl mx-auto w-2/3 md:w-80'/>
+                <img src={heroImage} alt='My profile' className=' rounded-2xl mx-auto w-1/2 md:w-80'/>
             </div>
         </div>
     </div>
